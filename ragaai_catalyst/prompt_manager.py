5b3d728424144fe2e1f47d64486b74ecee451134
Siddhartha Kosti <siddharthakosti@Siddharthas-MacBook-Pro.local>
2024-09-12 08:52:40 +0530

    feat: add get_parameter function and update prompt format return

--- ragaai_catalyst/prompt_manager.py
+++ ragaai_catalyst/prompt_manager.py
@@ -3,6 +3,7 @@ import requests
 import json
 import re
 from .ragaai_catalyst import RagaAICatalyst
+import pdb
 
 class PromptManager:
     NUM_PROJECTS = 100
@@ -174,6 +175,37 @@ class Prompt:
         except (KeyError, json.JSONDecodeError) as e:
             raise ValueError(f"Error parsing prompt list: {str(e)}")
 
+    def get_response_by_version(self, base_url, headers, timeout, prompt_name, version):
+        """
+        Get a specific version of a prompt.
+
+        Args:
+            base_url (str): The base URL for the API.
+            headers (dict): The headers to be used in the request.
+            timeout (int): The timeout for the request.
+            prompt_name (str): The name of the prompt.
+            version (str): The version of the prompt."""
+        try:
+            response = requests.get(f"{base_url}/version/{prompt_name}?version={version}",
+                                    headers=headers, timeout=timeout)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise requests.RequestException(f"Error fetching prompt version: {str(e)}")
+        except (KeyError, json.JSONDecodeError, IndexError) as e:
+            raise ValueError(f"Error parsing prompt version: {str(e)}")
+        return response
+
+    def get_response(self, base_url, headers, timeout, prompt_name, version):
+        try:
+            response = requests.get(f"{base_url}/version/{prompt_name}",
+                                headers=headers, timeout=timeout)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise requests.RequestException(f"Error fetching prompt version: {str(e)}")
+        except (KeyError, json.JSONDecodeError, IndexError) as e:
+            raise ValueError(f"Error parsing prompt version: {str(e)}")
+        return response
+
     def get_prompt_by_version(self, base_url, headers, timeout, prompt_name, version):
         """
         Get a specific version of a prompt.
@@ -191,18 +223,9 @@ class Prompt:
         Raises:
             requests.RequestException: If there's an error with the API request.
         """
-        try:
-            response = requests.get(f"{base_url}/version/{prompt_name}?version={version}",
-                                    headers=headers, timeout=timeout)
-            response.raise_for_status()
-            prompt = ""
-            for text_field in response.json()["data"]["docs"][0]["textFields"]:
-                prompt += text_field['content']
-            return prompt
-        except requests.RequestException as e:
-            raise requests.RequestException(f"Error fetching prompt version: {str(e)}")
-        except (KeyError, json.JSONDecodeError, IndexError) as e:
-            raise ValueError(f"Error parsing prompt version: {str(e)}")
+        response = self.get_response_by_version(base_url, headers, timeout, prompt_name, version)
+        prompt_text = response.json()["data"]["docs"][0]["textFields"]
+        return prompt_text
 
     def get_prompt(self, base_url, headers, timeout, prompt_name, version=None):
         """
@@ -221,22 +244,18 @@ class Prompt:
         Raises:
             requests.RequestException: If there's an error with the API request.
         """
-        try:
-            if version:
-                prompt_text = self.get_prompt_by_version(base_url, headers, timeout, prompt_name, version)
-            else:
-                response = requests.get(f"{base_url}/version/{prompt_name}",
-                                    headers=headers, timeout=timeout)
-                response.raise_for_status()
-                prompt_text = ""
-                for text_field in response.json()["data"]["docs"][0]["textFields"]:
-                    prompt_text += text_field['content']
-            
-            return PromptObject(prompt_text)
-        except requests.RequestException as e:
-            raise requests.RequestException(f"Error fetching prompt: {str(e)}")
-        except (KeyError, json.JSONDecodeError, IndexError) as e:
-            raise ValueError(f"Error parsing prompt: {str(e)}")
+        if version:
+            response = self.get_response_by_version(base_url, headers, timeout, prompt_name, version)
+            prompt_text = response.json()["data"]["docs"][0]["textFields"]
+            prompt_parameters = response.json()["data"]["docs"][0]["modelSpecs"]["parameters"]
+            model = response.json()["data"]["docs"][0]["modelSpecs"]["model"]
+        else:
+            response = self.get_response(base_url, headers, timeout, prompt_name)
+            prompt_text = response.json()["data"]["docs"][0]["textFields"]
+            prompt_parameters = response.json()["data"]["docs"][0]["modelSpecs"]["parameters"]
+            model = response.json()["data"]["docs"][0]["modelSpecs"]["model"]
+        return PromptObject(prompt_text, prompt_parameters, model)
+
 
     def list_prompt_versions(self, base_url, headers, timeout, prompt_name):
         """
@@ -268,16 +287,21 @@ class Prompt:
         except (KeyError, json.JSONDecodeError) as e:
             raise ValueError(f"Error parsing prompt versions: {str(e)}")
 
+
 class PromptObject:
-    def __init__(self, text):
+    def __init__(self, text, parameters, model):
         """
         Initialize a PromptObject with the given text.
 
         Args:
             text (str): The text of the prompt.
+            parameters (dict): The parameters of the prompt.
+            model (str): The model of the prompt.
         """
         self.text = text
         self.variables = self._extract_variables()
+        self.parameters = parameters
+        self.model = model
 
     def _extract_variables(self):
         """
@@ -286,7 +310,8 @@ class PromptObject:
         Returns:
             list: A list of variable names found in the prompt text.
         """
-        return [var.strip('{}') for var in self.text.split('{{')[1:]]
+        user_content = next(item["content"] for item in self.text if item["role"] == "user")
+        return [var.strip('{}') for var in user_content.split('{{')[1:]]
     
     def compile(self, **kwargs):
         """
@@ -311,12 +336,16 @@ class PromptObject:
             raise ValueError(f"Missing variable(s): {', '.join(missing_variables)}")
         if extra_variables:
             raise ValueError(f"Extra variable(s) provided: {', '.join(extra_variables)}")
+        # pdb.set_trace()
 
-        compiled_prompt = self.text
+        # compiled_prompt = self.text
+        user_content = next(item["content"] for item in self.text if item["role"] == "user")
+
         for key, value in kwargs.items():
             if not isinstance(value, str):
                 raise ValueError(f"Value for variable '{key}' must be a string, not {type(value).__name__}")
-            compiled_prompt = compiled_prompt.replace(f"{{{{{key}}}}}", value)
+            user_content = user_content.replace(f"{{{{{key}}}}}", value)
+        compiled_prompt = [{"content": user_content if item["role"] == "user" else item["content"], "role": item["role"]} for item in self.text]
         return compiled_prompt
     
     def get_variables(self):
@@ -327,5 +356,27 @@ class PromptObject:
             list: A list of variable names found in the prompt text.
         """
         pattern = r'\{\{(.*?)\}\}'
-        matches = re.findall(pattern, self.text)
+        user_content = next(item["content"] for item in self.text if item["role"] == "user")
+        matches = re.findall(pattern, user_content)
         return [match.strip() for match in matches if '"' not in match]
+    
+    # Function to convert value based on type
+    def convert_value(self, value, type_):
+        if type_ == "float":
+            return float(value)
+        elif type_ == "int":
+            return int(value)
+        return value  # Default case, return as is
+
+    def get_parameters(self):
+        """
+        Get all parameters in the prompt text.
+
+        Returns:
+            dict: A dictionary of parameters found in the prompt text.
+        """
+        parameters = {param["name"]: self.convert_value(param["value"], param["type"]) for param in self.parameters}
+        parameters["model"] = self.model
+        return parameters
+    
+    
