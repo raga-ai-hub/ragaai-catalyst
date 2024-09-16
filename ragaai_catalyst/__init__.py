da3964eb84b4083d865e292c63492478770177d9
Siddhartha Kosti <siddhartha.kosti@raga.ai>
2024-09-16 14:34:17 +0530

    add: import for PromptManager

--- ragaai_catalyst/__init__.py
+++ ragaai_catalyst/__init__.py
@@ -3,5 +3,6 @@ from .ragaai_catalyst import RagaAICatalyst
 from .tracers import Tracer
 from .utils import response_checker
 from .dataset import Dataset
+from .prompt_manager import PromptManager
 
-__all__ = ["Experiment", "RagaAICatalyst", "Tracer"]
+__all__ = ["Experiment", "RagaAICatalyst", "Tracer", "PromptManager"]
